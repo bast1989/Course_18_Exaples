ad619eb5202bee7ba0bc07b0dfed93f88ff0b0e8
Aleksandr Markov <bast.ne@mail.ru>
2023-02-22 14:15:41 +0700

    Example 005 if else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователяя: ");
+string user_name = Console.ReadLine();
+
+if (user_name.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!!!");
+}
+else
+{
+    Console.WriteLine("Привет " + user_name);
+}
