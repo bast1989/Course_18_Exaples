8ed7ad4559dbe36cc9f9d81552a7f3575cf01443
Aleksandr Markov <bast.ne@mail.ru>
2023-02-26 20:58:00 +0700

    lesson 2

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 12, 31, 4, 15, 16, 4, 17, 18 };
+int n = array.Length;
+
+Console.WriteLine("Введите чило для поиска его позиции в массиве: ");
+
+int find = int.Parse(Console.ReadLine()!);
+int index = 0;
+
+while(index < n) 
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine("Введёное вами число находится на пизиции: " + index);
+        break;
+    }
+    index++;
+}
